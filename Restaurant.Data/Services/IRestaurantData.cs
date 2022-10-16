4b7f967ad90be96e9b1d5b858c92b454b983f53e
Dimitris Oikonomou <dimioiko96@gmail.com>
2022-10-16 14:52:50 +0300

    Enter some in memory data

---
+++ Restaurant.Data/Services/IRestaurantData.cs
@@ -0,0 +1,14 @@
+﻿using RestaurantApp.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantApp.Data.Services
+{
+    interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+
+    }
+}
